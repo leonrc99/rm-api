6f3a1aef25bae1384b15c70d5ff24c67211366f7
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2024-10-28 18:24:21 -0300

    fix: fix pre authorize in ProductController

--- src/main/java/br/com/reliquiasdamagia/api/controller/ProductController.java
+++ src/main/java/br/com/reliquiasdamagia/api/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -17,21 +18,21 @@ public class ProductController {
 
     // Endpoint para criar produto (apenas ADMIN)
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         return ResponseEntity.ok(productService.createProduct(product));
     }
 
     // Endpoint para obter todos os produtos (acessível para USER, CONSULTANT e ADMIN)
     @GetMapping
-    @PreAuthorize("hasAnyRole('USER', 'CONSULTANT', 'ADMIN')")
+    @PreAuthorize("hasAnyAuthority('USER', 'CONSULTANT', 'ADMIN')")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
     // Endpoint para atualizar produto (apenas ADMIN)
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
         Product updatedProduct = productService.updateProduct(id, product);
         return ResponseEntity.ok(updatedProduct);
@@ -39,9 +40,17 @@ public class ProductController {
 
     // Endpoint para deletar produto (apenas ADMIN)
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Endpoint para obter um produto específico por ID (acessível para USER, CONSULTANT e ADMIN)
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('USER', 'CONSULTANT', 'ADMIN')")
+    public ResponseEntity<Optional<Product>> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
     }
 }
