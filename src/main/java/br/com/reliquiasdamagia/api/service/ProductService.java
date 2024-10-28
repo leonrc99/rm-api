2b67d83db56f31facbe21a30468e2cefdc6727ad
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2024-10-28 16:20:04 -0300

    feat: creating ProductService

---
+++ src/main/java/br/com/reliquiasdamagia/api/service/ProductService.java
@@ -0,0 +1,41 @@
+package br.com.reliquiasdamagia.api.service;
+
+import br.com.reliquiasdamagia.api.entity.Product;
+import br.com.reliquiasdamagia.api.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        return productRepository.findById(id).map(existingProduct -> {
+            existingProduct.setName(product.getName());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setStock(product.getStock());
+            return productRepository.save(existingProduct);
+        }).orElseThrow(() -> new RuntimeException("Produto não encontrado com o id " + id));
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
